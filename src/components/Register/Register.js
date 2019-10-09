c22d039c5526edf9254bf3deabf6843dd3d35afe
Sasha Amirov <amirovalex98@gmail.com>
2019-10-09 15:54:16 +0300

    updating register js file

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -9,7 +9,7 @@ const Register = ({onRouteChange,onSignIn}) => {
 					<p className="f3 pv2">Register:</p>
 					<div className="flexer pv1"><p>Name:</p><input className="log" type="text" name="name" /></div>
 					<div className="flexer pv1"><p>Email:</p><input className="log" type="email" name="login" /></div>
-					<div className="flexer pv1"><p>Password:</p><input className="pass" type="password" maxlength='16' name="password" /></div>
+					<div className="flexer pv1"><p>Password:</p><input className="pass" type="password" maxLength='16' name="password" /></div>
 					<div>
 					<div onClick={() => {onRouteChange('home'); onSignIn()}} className="pointer hover-bg-white-20 logbutt mh1 f6 link ph3 pv2 mb2 dib white bg-black">Register</div>
 					<div onClick={() => onRouteChange('signin')}className="hover-bg-white-20 pointer logbutt f6 mh1 link ph3 pv2 mb2 dib white bg-black">Sign In</div>
