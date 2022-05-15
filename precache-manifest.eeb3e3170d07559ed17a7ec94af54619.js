87ad49b4a3d193547d1169c750486049b120ed59
Sasha Amirov <amirovalex98@gmail.com>
2022-05-15 14:06:24 +0300

    Updates

---
+++ precache-manifest.eeb3e3170d07559ed17a7ec94af54619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e474fe48cbe44cba0a16579055c7dece",
+    "url": "/reactstore/index.html"
+  },
+  {
+    "revision": "9d9fe763d060f1438c82",
+    "url": "/reactstore/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "16743cbfebf45cf0547c",
+    "url": "/reactstore/static/css/main.906ec529.chunk.css"
+  },
+  {
+    "revision": "9d9fe763d060f1438c82",
+    "url": "/reactstore/static/js/2.c3ea047d.chunk.js"
+  },
+  {
+    "revision": "16743cbfebf45cf0547c",
+    "url": "/reactstore/static/js/main.285e0a5a.chunk.js"
+  },
+  {
+    "revision": "cf25f07586f290ffe9db",
+    "url": "/reactstore/static/js/runtime-main.13b25107.js"
+  },
+  {
+    "revision": "0c01e5b48bc5b19b978e546dc3850eb0",
+    "url": "/reactstore/static/media/navLogo.0c01e5b4.png"
+  }
+]);
